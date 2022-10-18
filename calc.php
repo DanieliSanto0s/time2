a1570794d61bf8a1858147a7dd8008e308236611
AnaMorata <90793364+AnaMorata8642@users.noreply.github.com>
2022-10-18 11:36:29 -0300

    Update calc.php

--- calc.php
+++ calc.php
@@ -6,18 +6,13 @@ $calculo = $valor + $Segvalor;
 
 echo "a soma dos dois valores selecionados será: " . $calculo ;
 
-
-
-if ($calculo >= 20) {
-    
+if ($calculo >= 20)
+{
     echo "a soma do valor que digitou era maior que vinte, então acrescentado mais 8 a soma fica: " . $calculo + 8;
 }
 
 else
 {
-    
-
     echo "a soma do valor que digitou era menor que vinte, então será tirado -5 fica: " . $calculo - 5;
 }
-
 ?>
