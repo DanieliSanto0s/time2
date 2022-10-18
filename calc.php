a6e2f15ac26f9ad96c6e87efb29270681b6b47e6
Danii <85881097+DanieliSanto0s@users.noreply.github.com>
2022-10-18 08:00:57 -0300

    Add files via upload

---
+++ calc.php
@@ -0,0 +1,23 @@
+<?php
+
+$valor = $_POST['Pnum'];
+$Segvalor = $_POST['Snum'];
+$calculo = $valor + $Segvalor;
+
+echo "a soma dos dois valores será: " . $calculo ;
+
+
+
+if ($calculo >= 20) {
+    
+    echo "a soma do valor que digitou era maior que vinte, então acrescentado mais 8 a soma fica: " . $calculo + 8;
+}
+
+else
+{
+    
+
+    echo "a soma do valor que digitou era menor que vinte, então será tirado -5 fica: " . $calculo - 5;
+}
+
+?>
